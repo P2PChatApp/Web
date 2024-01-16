59589122d79f6ea5ec0821f153ee4cfa1118dd0b
Taka005 <01taka005@gmail.com>
2024-01-16 18:05:57 +0900

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 const System = new System();
+
+const NameForm = document.getElementById("NameForm");
+const NameInput = document.getElementById("NameInput");
+const NameButton = document.getElementById("NameButton");
+
+//名前の変更処理
+NameButton.addEventListener("click",()=>{
+  if(NameInput.disabled){
+    NameInput.disabled = false;
+    NameButton.value = "保存";
+  }else{
+    NameInput.disabled = true;
+    NameButton.value = "編集";
+  }
+});
