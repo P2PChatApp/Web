ebda8beb4885d3a73f4391a53dd15bf8002b35e6
Taka005 <01taka005@gmail.com>
2024-01-17 08:16:41 +0900

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,9 @@ const log = document.getElementById("log");
 const NameInput = document.getElementById("NameInput");
 const NameButton = document.getElementById("NameButton");
 
-//名前の変更処理
+//名前の処理
+NameInput.value = system.client.name;
+
 NameButton.addEventListener("click",(event)=>{
   event.preventDefault();
 
@@ -20,6 +22,8 @@ NameButton.addEventListener("click",(event)=>{
       NameInput.value.length < 4||
       NameInput.value.length > 8
     ) return log.innerText = "名前は4以上8文字以内に指定してください";
+
+    system.client.name = NameInput.value;
 
     NameInput.disabled = true;
     NameButton.classList.replace("btn-primary","btn-success")
