d5000f55a949e159130ef0a8af358ed41c6955f0
Taka005 <01taka005@gmail.com>
2024-01-21 20:20:59 +0900

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,6 +243,7 @@ function escape(str){
 function formatDate(date){
   const hours = String(date.getHours()).padStart(2,"0");
   const minutes = String(date.getMinutes()).padStart(2,"0");
+  const seconds = String(data.getSeconds()).padStart(2, '0');
 
-  return `${hours}:${minutes}`;
+  return `${hours}:${minutes}:${seconds}`;
 }
